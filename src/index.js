0c830d83895f00c219746babc090b514839a1140
AinzsOalGoan <deep951india@gmail.com>
2025-04-13 23:06:15 +0530

    user controller and route initialized also fixed error of creating two instances of express app in app and index

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import dotenv from "dotenv";
-import express from "express";
+import { app } from "./app.js"; // we r using app.js app not directly from express which will create a new instance of express and will make us lose all the made up routes, middleware built in app.js
 import connectDB from "./db/index.js";
 
 dotenv.config({
     path: "./env",
 });
 
-const app = express();
 
 connectDB()
     .then(() => {
         app.listen(process.env.PORT || 8000, () => {
-            console.log(` Server is running at port ${process.env.PORT}`);
+            console.log(`⚙️  Server is running at port ${process.env.PORT}`);
         });
         app.on("error", (error) => {
             console.log("Server error:", error);
