618e7315343f7f824204a291f14ab9023b7fc8cc
AinzsOalGoan <deep951india@gmail.com>
2025-04-19 22:18:52 +0530

    added user routes and fetched watch history

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.models.js";
 import { uploadOnCloudinary } from "../utils/coudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 const generateTokens = async (userId) => {
     try {
@@ -409,14 +410,61 @@ const getUserChanneProfile = asyncHandler(async (req, res) => {
             },
         },
     ]);
-    if(!channel?.length){
-        throw new ApiError(404,"Channel doesn't exist")
+    if (!channel?.length) {
+        throw new ApiError(404, "Channel doesn't exist");
     }
     return res
+        .status(200)
+        .json(new ApiResponse(200, channel[0], "User channel fetched"));
+});
+
+const getWatchHistory = asyncHandler(async (req, res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id),
+            },
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as:"watchHistory",
+                pipeline:[
+                    {
+                        $lookup:{
+                            from:"users",
+                            localField:"owner",
+                            foreignField:"_id",
+                            as:"owner",
+                            pipeline:[
+                                {
+                                    $project:{
+                                        fullName:1,
+                                        username:1,
+                                        avatar:1
+
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields:{
+                            owner:{
+                                $first:"$owner"
+                            }
+                        }
+                    }
+                ]
+            },
+        },
+    ]);
+
+    return res
     .status(200)
-    .json(
-        new ApiResponse(200,channel[0],"User channel fetched")
-    )
+    .json(new ApiResponse(200,user[0].watchHistory,"Watch history is fetched"))
 });
 
 export {
@@ -430,4 +478,5 @@ export {
     updateUserAvatar,
     updateUserCoverImage,
     getUserChanneProfile,
+    getWatchHistory,
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import {
+    changeCurrentPassword,
     loginUser,
     logoutUser,
     refreshAccessToken,
     registerUser,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChanneProfile,
+    getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -28,5 +35,17 @@ router.route("/login").post(loginUser);
 
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
-router.route("/refresh-token").post( refreshAccessToken);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/change-detail").patch(verifyJWT, updateAccountDetails);
+router
+    .route("/change-avatar")
+    .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+    .route("/change-coverimage")
+    .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChanneProfile);
+router.route("/watch-history").get(verifyJWT, getWatchHistory);
+
 export default router;
